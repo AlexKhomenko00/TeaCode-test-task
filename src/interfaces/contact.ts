a15284078793365d6c7460d4846dd6a4f40dacff
Alex Khomenko <sasvsha@gmail.com>
2021-09-13 19:36:45 +0200

    updated contacts interfaces

--- src/interfaces/contact.ts
+++ src/interfaces/contact.ts
@@ -1,8 +1,12 @@
-export interface IContact {
+export interface IFetchedContact {
 	id: number;
 	first_name: string;
 	last_name: string;
 	email: string;
 	gender: string;
 	avatar: string;
+}
+
+export interface IContact extends IFetchedContact {
+	checked: boolean;
 }
