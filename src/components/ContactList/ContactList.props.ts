7b7680735d87b516ab3af8f734651a5e88be3ca2
Alex Khomenko <sasvsha@gmail.com>
2021-09-13 17:34:26 +0200

    initialized reusable contacts list

---
+++ src/components/ContactList/ContactList.props.ts
@@ -0,0 +1,5 @@
+import { IContact } from "interfaces/contact";
+
+export interface ContactItemProps extends IContact {
+	contacts: IContact[];
+}
---
+++ src/components/ContactList/ContactList.tsx
@@ -0,0 +1,7 @@
+import s from "./ContactList.module.css";
+
+const ContactList = () => {
+	return <ul></ul>;
+};
+
+export default ContactList;
